46cf92ff63a14b33f9bf5c9b70f6c2c67ba99a42
FinnKrause <finn_krause@gmx.de>
2023-04-30 02:15:57 +0200

    initalCommit

---
+++ README.md
@@ -0,0 +1,20 @@
+# AR App DM
+
+## Allgemeines
+Das hier ist eine App entwickelt für den Teamstand von RecoilRacing für die Deutsche Meisterschaft. Der Code ist auf nachfrage bei recoilracinggermany@gmail.com verwendbar.
+Die App ist für Android gedacht.
+
+## Nutzung
+### Software Versionen
+Unity Version: 2021.3.15f1\
+Vuforia Version 10.11.3\
+High Definition RP Version 12.1.8\
+
+### Set-Up
+1. Unity installieren
+2. Neues Projekt erstellen
+3. Vuforia Package von der Website herundterladen in Unity installieren
+4. Public Vuforia Liscense Key hinzufügen
+5. git Repository im Assets Folder initialisieren 
+6. Build probieren und Android spezifische Fehlermedlungen Beheben (API Lever, ARM64)
+7. Hoffen
---
+++ Scripts/Teambox/Click_Karte.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Click_Karte : MonoBehaviour
+{
+    GameObject Video;
+    GameObject VideoPlayerObject;
+    VideoPlayer VideoPlayer;
+    GameObject PlayButton;
+    Animator VideoAnimator;
+
+    public VideoClip caro;
+    public VideoClip benno;
+    public VideoClip timon;
+    public VideoClip marleen;
+    public VideoClip amelie;
+    public VideoClip finn;
+
+    public enum Names
+    {
+        AMELIE, TIMON, CAROLINE, BENNO, MARLEEN, FINN, UNKNOWN
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayerObject = GameObject.Find("VideoPlayer");
+
+        Debug.Log(VideoPlayerObject is not null);
+
+
+        if (VideoPlayerObject != null)
+        {
+            VideoPlayer = VideoPlayerObject.GetComponent<VideoPlayer>();
+        }
+
+        PlayButton = GameObject.Find("PlayButton");
+
+        Video = GameObject.Find("Video");
+        VideoAnimator = Video.GetComponent<Animator>();
+        Video.SetActive(false);
+    }
+
+    private void handleOnDisapear()
+    {
+        Video.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+
+
+                if (hit.transform.name.Contains("Target Representation"))
+                {
+                    Names personClicked = getPersonFromClick(hit.transform.name);
+                    setVideoSourceFromName(personClicked);
+
+                    Video.SetActive(true);
+                    VideoPlayer.Play();
+                    VideoPlayer.Pause();
+                    VideoPlayer.frame = 10;
+
+                    VideoAnimator.SetInteger("Teammitglied", (int)personClicked + 1);
+
+                    PlayButton.SetActive(true);
+                }
+                else if (hit.transform.name == "Video Collider")
+                {
+                    if (VideoPlayer.isPlaying == false)
+                    {
+                        VideoPlayer.Play();
+                        PlayButton.SetActive(false);
+                    }
+                    else
+                    {
+                        VideoPlayer.Pause();
+                        PlayButton.SetActive(true);
+                    }
+                }
+            }
+            else
+            {
+                VideoAnimator.SetInteger("Teammitglied", 0);
+                Video.SetActive(false);
+            }
+        }
+
+    }
+
+    protected Names getPersonFromClick(string elementName)
+    {
+        switch (elementName.Split(" ")[elementName.Split(" ").Length - 1])
+        {
+            case "Caroline":
+                return Names.CAROLINE; break;
+            case "Amelie":
+                return Names.AMELIE; break;
+            case "Benno":
+                return Names.BENNO; break;
+            case "Marleen":
+                return Names.MARLEEN; break;
+            case "Timon":
+                return Names.TIMON; break;
+            case "Finn":
+                return Names.FINN; break;
+            default:
+                return Names.UNKNOWN;
+        }
+    }
+
+    private void setVideoSourceFromName(Names name)
+    {
+        switch (name)
+        {
+            case Names.CAROLINE:
+                VideoPlayer.clip = caro; break;
+            case Names.AMELIE:
+                VideoPlayer.clip = amelie; break;
+            case Names.BENNO:
+                VideoPlayer.clip = benno; break;
+            case Names.FINN:
+                VideoPlayer.clip = finn; break;
+            case Names.MARLEEN:
+                VideoPlayer.clip = marleen; break;
+            case Names.TIMON:
+                VideoPlayer.clip = timon; break;
+        }
+    }
+}
